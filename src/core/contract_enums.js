0e930ebe9bea42ddcc90f123139474c66c168707
widiskel <waputra16@gmail.com>
2024-07-29 14:01:46 +0700

    fix (contract) fix TON JETTON Contract address

--- src/core/contract_enums.js
+++ src/core/contract_enums.js
@@ -1,3 +1,3 @@
 export class ContractEnums {
-  static SWAPCONTRACT = "kQBOnpVBzYFYhfk8D8udcnuMkz0tssFHNZbRt1G6-_q51jN8";
+  static TONJETTONCONTRACT = "kQBOnpVBzYFYhfk8D8udcnuMkz0tssFHNZbRt1G6-_q51jN8";
 }
